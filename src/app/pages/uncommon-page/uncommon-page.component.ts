9846248216bdc35ca201e2767eddc77ea1759899
Juan20Daniel <juandaniel312000@gmail.com>
2025-04-29 22:31:53 -0600

    Pipe básicos

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
-import { I18nPluralPipe, I18nSelectPipe } from '@angular/common';
+import { AsyncPipe, I18nPluralPipe, I18nSelectPipe, JsonPipe, KeyValuePipe, SlicePipe, UpperCasePipe } from '@angular/common';
+import { interval, tap } from 'rxjs';
 
 const client1 = {
   name: 'Carlos',
@@ -15,9 +16,21 @@ const client2 = {
   address: 'Las joyas de guadalajara'
 }
 
+// Pipes de internasionalización
+// I18nSelectPipe, I18nPluralPipe
+
 @Component({
   selector: 'app-uncommon-page',
-  imports: [ CardComponent, I18nSelectPipe ],
+  imports: [
+    CardComponent,
+    I18nSelectPipe,
+    I18nPluralPipe,
+    SlicePipe,
+    JsonPipe,
+    UpperCasePipe,
+    KeyValuePipe,
+    AsyncPipe
+  ],
   templateUrl: './uncommon-page.component.html'
 })
 export default class UncommonPageComponent {
@@ -36,4 +49,45 @@ export default class UncommonPageComponent {
     }
     this.client.set(client1);
   }
+
+  clientsMap = signal({
+    '=0':'no tenemos nungún cliente esperando',
+    '=1':'tenemos un cliente esperando',
+    '=2':'tenemos 2 clientes esperando',
+    other:'tenemos # clientes esperando'
+  })
+
+  clients = signal([
+    'Maria',
+    'Pedro',
+    'Juan',
+    'Marco',
+    'Melisso',
+    'Fernando',
+    'Natalia',
+    'Andrea'
+  ]);
+  deleteClient() {
+    this.clients.update(prev => prev.slice(1));
+  }
+
+  // KeyValuePipe
+
+  profile = {
+    name:'Fernanflow',
+    age:2,
+    address: 'Ottawa yo keni chawa'
+  }
+
+  // AsyncPipe
+  promiseValue:Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Giuston tenemos un problema');
+      console.log('promesa finalizada')
+    },3500)
+  });
+
+  myObservableTimer = interval(1000).pipe(
+    tap((value) => console.log('tap', value))
+  )
 }
